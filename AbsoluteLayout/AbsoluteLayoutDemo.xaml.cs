391adc5f217edd30d59b438f4c1bc2513e1de9c7
ERIKA TAMARA GARCIA <oscary_1320114020@uptecamac.edu.mx>
2023-01-24 11:57:51 -0600

    24.01.23

---
+++ AbsoluteLayout/AbsoluteLayoutDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace AbsoluteLayout;
+
+public partial class AbsoluteLayoutDemo : ContentPage
+{
+	public AbsoluteLayoutDemo()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AbsoluteLayout/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AbsoluteLayout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
